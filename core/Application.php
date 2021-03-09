fac52a9b32805fb7393dfe32fcb42bd4f93b96c2
Ian Mubangizi <io@ianmubangizi.com>
2021-03-09 23:09:27 +0300

    update Application.php

--- core/Application.php
+++ core/Application.php
@@ -7,12 +7,13 @@ class Application {
   public Router $router;
   public Request $request;
   public static string $ROOT_DIR;
+  public static Application $app;
   
   public function __construct($root_dir_path){
+    self::$app = $this;
     self::$ROOT_DIR = $root_dir_path;
     $this->request = new Request();
     $this->router = new Router($this->request);
-    
   }
   
   public function run(){
