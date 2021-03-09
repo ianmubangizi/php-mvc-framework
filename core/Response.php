7c6cb806ddb742266f99298d94463212e0ad90f7
Ian Mubangizi <io@ianmubangizi.com>
2021-03-09 23:03:18 +0300

    update Response.php

---
+++ core/Response.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Response
+{
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function set_status_code(int $code){
+        http_response_code($code);
+    }
+}
+
+
+?>
