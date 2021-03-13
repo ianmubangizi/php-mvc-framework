4a596df106c30e3f947fc58676099cbf880d4191
Ian Mubangizi <io@ianmubangizi.com>
2021-03-13 16:00:34 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,15 +2,14 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+use framework\controllers\SiteController;
 use framework\core\Application;
 
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
 $app->router->get('/contact', 'contact');
-$app->router->post('/contact', function(){
-   return "Submitting Message";
-});
+$app->router->post('/contact', [SiteController::class, 'handle_contant_submit']);
 
 $app->run();
 
