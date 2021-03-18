34929970483538e2d055236c9575e6401afb3adc
Ian Mubangizi <io@ianmubangizi.com>
2021-03-18 22:53:08 +0300

    update View.php

--- core/View.php
+++ core/View.php
@@ -3,5 +3,26 @@
 namespace framework\core;
 
 class View {
-    
+    public function render($view, $params = []){
+      $layout = $this->get_layout();
+      $content = $this->get_view_content($view, $params);
+      return str_replace('{{content}}', $content, $layout);
+    }
+  
+    protected function get_layout(){
+      ob_start();
+      include_once(Application::$ROOT_DIR . "/views/layouts/main.php");
+      return ob_get_clean();
+    }
+  
+  
+    protected function get_view_content($view, $params = []){
+      foreach($params as $key => $value){
+        $$key = $value;
+      }
+      ob_start();
+      include_once(Application::$ROOT_DIR . "/views/$view.php");
+      return ob_get_clean();
+    }
+}
 }
