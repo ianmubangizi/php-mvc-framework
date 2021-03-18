48ad6c44b69bc6732365ccf4580d186de82857dc
Ian Mubangizi <io@ianmubangizi.com>
2021-03-18 23:44:08 +0300

    update Controller.php

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -2,12 +2,14 @@
 
 namespace framework\controllers;
 
+use framework\core\View;
+
 abstract class Controller {
     
     protected View $view;
     
-    public function __construct(View $view = null){
-       $this->view = $view === null ? new View() : $view;
+    public function __construct(View $view = new View){
+       $this->view = $view;
     }
    
     public function render($view, $params = []){
