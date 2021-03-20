899d89386041ab48db9ca5f2fa3ae6411488438f
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 23:06:03 +0300

    update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,7 +11,7 @@
     <title><?= $page_title ?? 'PHP MVC FRAMEWORK' ?></title>
   </head>
   <body>
-     <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
+     <header class="d-flex flex-row p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
        <p class="h5 my-0 me-md-auto fw-normal">Company</p>
        <nav class="my-2 my-md-0 me-md-3">
           <a class="p-2 text-dark" href="/contact">Contact Us</a>
