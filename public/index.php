78db6f896a1525d0d4485a985c11d3c0a8fcfd1b
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 22:08:56 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -2,14 +2,21 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-use framework\controllers\SiteController;
 use framework\core\Application;
+use framework\controllers\SiteController;
+use framework\controllers\AuthControllerAuthController;
 
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
 $app->router->get('/contact', 'contact');
 $app->router->post('/contact', [SiteController::class, 'handle_contact_submit']);
+
+$app->router->get('/auth/login', [AuthController::class, 'login']);
+$app->router->post('/auth/login', [AuthController::class, 'login']);
+$app->router->get('/auth/register', [AuthController::class, 'register']);
+$app->router->get('/auth/register', [AuthController::class, 'register']);
+
 
 $app->run();
 
