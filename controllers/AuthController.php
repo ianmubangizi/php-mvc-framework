ca3d2eac0a8dd628d147fb6eb9b3f87b9709f617
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 22:53:26 +0300

    update AuthController.php

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -5,10 +5,10 @@ namespace framework\controllers;
 class AuthController {
     
     public function login(){
-        
+        return $this->render('auth/login');
     }
     
     public function register(){
-        
+        return $this->render('auth/register');
     }
 }
