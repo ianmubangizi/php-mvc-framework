10bd818ea1e01a410836f46fbebd5e1c3b511c89
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 22:59:26 +0300

    update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>PHP MVC<?= $page_title ?></title>
+    <title>PHP MVC<?= ' | ' . $page_title ?? null ?></title>
   </head>
   <body>
      <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
