5d052019d8083094155e8ae4d6dd440c34358b66
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 20:56:50 +0300

    update Request.php

--- core/Request.php
+++ core/Request.php
@@ -18,7 +18,21 @@ class Request {
   }
   
   public function get_body(){
+      $body = [];
       
+      if($this->get_method() === 'get'){
+          foreach ($_GET as $key => $value) {
+             $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+          }
+      }
+      
+      if($this->get_method() === 'post'){
+          foreach ($_POST as $key => $value) {
+             $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+          }
+      }
+      
+      return $body;
   }
 }
 
