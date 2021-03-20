51e1889a23e3bc64034867d2288975fe264017ee
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 23:37:02 +0300

    update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -48,8 +48,8 @@
       </div>
     </nav>
     <main class="container py-2">
-      {{content}}
-      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    {{content}}
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
           <div class="col-12 col-md">
             <small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
@@ -84,7 +84,7 @@
             </ul>
           </div>
         </div>
-      </footer>
+    </footer>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
