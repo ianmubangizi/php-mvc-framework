377b4ae0fa34629a8469bcf4a100e98437477a63
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 23:49:24 +0300

    update register.php

--- views/auth/register.php
+++ views/auth/register.php
@@ -3,7 +3,16 @@
 ?>
 
 <h1 class="center">Register</h1>
+<form class="row row-cols-lg-auto g-3 align-items-center">
+  <div class="col-12">
+    <label class="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
+    <div class="input-group">
+      <div class="input-group-text">@</div>
+      <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username">
+    </div>
+  </div>
 
-<form class="d-flex flex-column">
-    
+  <div class="col-12">
+    <button type="submit" class="d-flex text-center btn btn-primary">Submit</button>
+  </div>
 </form>
