1b3fd7a918ddb85316a7ac4405b4ce033a8d160f
Ian Mubangizi <io@ianmubangizi.com>
2021-03-20 21:42:18 +0300

    update SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,9 +2,15 @@
 
 namespace framework\controllers;
 
+use framework\core\Request;
+
 class SiteController extends Controller {
   
-  public function handle_contact_submit(){
+  public function handle_contact_submit(Request $request){
+      echo '<pre>';
+      var_dump($request->get_body());
+      echo '</pre>';
+      exit;
     return $this->render('contact', ['message' => 'submitting form']);
   }
 }
