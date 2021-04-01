7450d345f150b3fe8446fa0e08fc1c913a50f995
ianmubangizi <io@ianmubangizi.com>
2021-04-01 16:37:12 +0300

    use only mysql syntax, for mariadb 8.0

--- app.sql
+++ app.sql
@@ -1,11 +1,6 @@
--- Postgres syntax
-create table if not exists migrations (
-  id serial primary key,
-  migration varchar(255),
-  create_at timestamp default current_timestamp
-);
-
 -- MySQL syntax
+
+-- Create the migrations tabel
 create table if not exists migrations (
    id int(10) auto_increment primary key,
    migration varchar(255),
