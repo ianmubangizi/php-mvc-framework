c4cac2563d2005cb2aa416fe2036c678effff1f0
Ian Mubangizi <io@ianmubangizi.com>
2021-04-29 02:16:24 +0300

    form phase refactoring

--- Core/Form/Form.php
+++ Core/Form/Form.php
@@ -4,77 +4,109 @@ namespace Mubangizi\Core\Form;
 
 use Mubangizi\Core\Model;
 
-class Form {
-  
+class Form
+{
+
   protected Model $model;
-  
-  public function __construct(Model $model){
+
+  public function __construct(Model $model)
+  {
     $this->model = $model;
   }
-  
-  public static function open($method, $action = '', $classes = ['py-3']){
-  $classes = implode(' ', $classes);
-return <<<tag
-<form
- method="$method"
- class="$classes"
- action="$action"
->
-tag;
+
+  protected function get_value($name)
+  {
+    return $this->model->{$name};
   }
-  
-  public static function close(){
+
+  protected function get_error($name)
+  {
+    return ucfirst($this->model->get_error($name));
+  }
+
+  protected function get_invalid($name)
+  {
+    return $this->model->has_error($name)
+      ? 'is-invalid'
+      : null;
+  }
+
+  public static function open($method, $action = '', $classes = ['py-3'])
+  {
+    $classes = implode(' ', $classes);
+    return <<<tag
+      <form method="$method" class="$classes" action="$action" >
+    tag;
+  }
+
+  public static function close()
+  {
     return '</form>';
   }
-  
-  public function button($text, $type, $classes = ['btn', 'btn-md', 'btn-primary']){
-    $classes = implode(' ', $classes);
-return <<<tag
-  <button 
-    class="$classes"
-    type="type">
-    $text
-  </button>
-tag;
-  }
-  
-  public function input($label, $name, $type = 'text', string $icon = null){
-return <<<tag
-<div class="form-group">
-  <label for="$name">
-    $label
-  </label>
-  {$this->_input($type, $name, $icon)}
-</div>
-tag;
-  }
-  
-  public function _input($type, $name, $icon){
-    $value = $this->model->{$name};
-    $is_invalid = $this->model->has_error($name) 
-      ? 'is-invalid' 
-      : null;
-    $classes = implode(' ', ['form-control', $is_invalid]);
-    $input = <<<input
-<input
-  name="$name"
-  type="$type"
-  value="$value"
-  class="$classes">
-<div class="invalid-feedback">
-  {$this->model->get_error($name)}
-</div> 
-input;
 
-return $icon !== null
-? <<<tag
-<div class="input-group has-validation">
-  <div class="input-group-text">
-    <i class="fa fa-$icon"></i>
-  </div>
-  $input
-</div>
-tag
-: $input;
+  public function button($text, $type, $classes = ['btn', 'btn-md', 'btn-primary'])
+  {
+    $classes = implode(' ', $classes);
+    return <<<tag
+      <button class="$classes" type="$type">
+        $text
+      </button>
+    tag;
   }
-} 
+
+  public function input($label, $name, $type = 'text', string $icon = null)
+  {
+    return <<<tag
+      <div class="form-group">
+        <label for="$name">
+          $label
+        </label>
+        {$this->input_group($type,$name,$icon)}
+      </div>
+    tag;
+  }
+
+  protected function input_group($type, $name, $icon)
+  {
+    $classes = implode(' ', ['form-control', $this->get_invalid($name)]);
+    $input = <<<input
+      <input
+        name="$name"
+        type="$type"
+        value="{$this->get_value($name)}"
+        class="$classes">
+      <div class="invalid-feedback">
+        {$this->get_error($name)}
+      </div> 
+    input;
+
+    return $icon !== null ? <<<tag
+      <div class="input-group has-validation">
+        <div class="input-group-text">
+          <i class="fa fa-$icon"></i>
+        </div>
+        $input
+      </div>
+    tag : $input;
+  }
+
+  public function textarea($label, $name, $placeholder = '')
+  {
+    $classes = implode(' ', ['form-control', $this->get_invalid($name)]);
+    return <<<textarea
+      <div class="form-group">
+          <label for="$name">
+            $label
+          </label>
+          <div class="input-group has-validation">
+            <textarea name="$name" 
+              class="$classes"
+              placeholder="$placeholder">{$this->get_value($name)}</textarea>
+            <div class="invalid-feedback">
+              {$this->get_error($name)}
+            </div> 
+          </div>
+        </div>
+    textarea;
+  }
+}
