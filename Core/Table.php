37b8f4184ad795b74a4a3394d225e455fbf59d10
Ian Mubangizi <io@ianmubangizi.com>
2021-04-30 00:51:40 +0300

    check if a value exists in table

--- Core/Table.php
+++ Core/Table.php
@@ -12,6 +12,11 @@ abstract class Table extends Model
         return Application::$app->database->db;
     }
 
+    protected function primary_key()
+    {
+        return 'id';
+    }
+
     public abstract function table_name(): string;
     protected abstract function table_columns(): array;
 
@@ -44,5 +49,19 @@ abstract class Table extends Model
     public static function prepare(string $sql)
     {
         return static::db()->prepare($sql);
+    }
+
+    public function exists(int|string $value, string $column = null, string $table = null)
+    {
+        $key = $column !== null
+            ? $column
+            : $this->primary_key();
+        $table = $table !== null
+            ? $table
+            : $this->table_name();
+        $statement = $this->prepare("SELECT $key FROM $table WHERE $key = :param;");
+        $statement->bindValue(':param', $value);
+        $statement->execute();
+        return is_array($statement->fetch()) ? true : false;
     }
 }
