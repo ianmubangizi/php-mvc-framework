f2ee89518789408a173a20bb5a014b8800d9e036
Ian Mubangizi <io@ianmubangizi.com>
2021-04-29 20:20:30 +0300

    update auth user interface

--- views/auth/login.php
+++ views/auth/login.php
@@ -9,14 +9,18 @@ use Mubangizi\Core\Form\Form;
 $form = new Form($model);
 ?>
 
-<h1 class="text-center">
-  Sign into your account
-</h1>
+
 
 <?= $form::open('post') ?>
-<div class="d-grid col-12 col-md-6 col-sm-8 gap-3 p-2 mx-auto shadow bg-body rounded">
+<div class="d-grid col-md-5 col-lg-4 gap-4 p-4 mx-auto shadow bg-body rounded">
+  <h1>
+    Sign in
+  </h1>
+  <p>Provide your credentials and access your account.</p>
   <?= $form->input('Your email', 'email', 'email', 'envelope') ?>
   <?= $form->input('Password', 'password', 'password', 'lock') ?>
+  <a href="/auth/reset-password">Forgot Password?</a>
   <?= $form->button('Login', 'submit') ?>
 </div>
+<p class="text-center p-3">Are you new here? <a href="/auth/register">Register</a></p>
 <?= $form::close() ?>
--- views/auth/register.php
+++ views/auth/register.php
@@ -1,21 +1,24 @@
 <?php
-  /** @var Mubangizi\Models\User $model */
-  use Mubangizi\Core\Form\Form;
-  
-  $form = new Form($model);
+
+/** @var Mubangizi\Models\User $model */
+
+use Mubangizi\Core\Form\Form;
+
+$form = new Form($model);
 ?>
 
-<h1 class="text-center">
-  Sign up for your account
-</h1>
-
 <?= $form::open('post', '', ['py-2', 'm-3']) ?>
-  <div class="d-grid col-12 col-md-6 col-sm-8 gap-3 p-4 mx-auto shadow bg-body rounded">
-    <?= $form->input('First name', 'first_name') ?>
-    <?= $form->input('Last name', 'last_name') ?>
-    <?= $form->input('Your email', 'email', 'email', 'envelope') ?>
-    <?= $form->input('Password', 'password', 'password', 'lock') ?>
-    <?= $form->input('Confirmation', 'confirm_password', 'password', 'lock') ?>
-    <?= $form->button('Register', 'submit') ?>
-  </div>
+<div class="d-grid col-md-5 col-lg-4 gap-3 p-4 mx-auto shadow bg-body rounded">
+  <h1>
+    Register
+  </h1>
+  <p>Start using the application with an account</p>
+  <?= $form->input('First name', 'first_name') ?>
+  <?= $form->input('Last name', 'last_name') ?>
+  <?= $form->input('Your email', 'email', 'email', 'envelope') ?>
+  <?= $form->input('Password', 'password', 'password', 'lock') ?>
+  <?= $form->input('Confirmation', 'confirm_password', 'password', 'lock') ?>
+  <p>Already got an account? <a href="/auth/login">Login</a></p>
+  <?= $form->button('Register', 'submit') ?>
+</div>
 <?= $form::close() ?>
