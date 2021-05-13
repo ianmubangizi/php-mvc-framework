2a29f9b6ce07ed0c3be4aa55d46cb870d93ff10c
Ian Mubangizi <io@ianmubangizi.com>
2021-05-13 22:37:08 +0300

    added twig rendering

--- Core/View.php
+++ Core/View.php
@@ -2,21 +2,14 @@
 
 namespace Mubangizi\Core;
 
+use Mubangizi\Core\Widget\Widget;
+use Twig\Extension\EscaperExtension;
+
 class View
 {
 
   public $views;
   public $layout;
-  protected $params = [];
-  protected $partials = [
-    '{{footer}}' => 'layouts/partials/footer',
-    '{{navigation}}' => 'layouts/partials/navbar'
-  ];
-  protected $extras = [
-    '{{meta_tags}}' => [],
-    '{{stylesheet}}' => [],
-    '{{javascript}}' => []
-  ];
 
   public function __construct($layout = 'main')
   {
@@ -24,76 +17,21 @@ class View
     $this->views = Application::$ROOT_DIR . '/views';
   }
 
-  public function add_extra(string $tag, string ...$tags)
-  {
-    array_merge(
-      $this->extras["{{$tag}}"],
-      $tags
-    );
-  }
-
-  public function set_html($partial, $markup)
-  {
-    $this->partials[$partial] = $markup;
-  }
-
   public function render($view, $params = [])
   {
-    $this->params = $params;
-    $exists = file_exists("{$this->views}/$view.php");
-    $view = $exists ? $view : 'error';
+    static $twig = null;
 
-    if (!$exists) {
-      $params = [
-        'code' => 500,
-        'message' => 'Server error'
-      ];
+    if ($twig === null) {
+      $loader = new \Twig\Loader\FilesystemLoader();
+      $loader->setPaths([$this->views, "{$this->views}/layouts"]);
+
+      $twig = new \Twig\Environment($loader);
+
+      /** @var EscaperExtension */
+      $escaper = $twig->getExtension(EscaperExtension::class);
+      $escaper->addSafeClass(Widget::class, ['html']);
     }
 
-    $layout = str_replace(
-      '{{view_layout}}',
-      $this->get_layout($this->layout),
-      $this->get_layout('base')
-    );
-
-    $this->set_html('{{content}}', $this->get_view($view));
-
-    foreach ($this->partials as $partial => $value) {
-      $file = "{$this->views}/$value.php";
-      if ($partial !== '{{content}}') {
-        $value = file_exists($file)
-          ? $this->load($file)
-          : '';
-      }
-      $layout = str_replace($partial, $value, $layout);
-    }
-
-    foreach ($this->extras as $extra => $tags) {
-      $layout = str_replace($extra, array_reduce($tags, function ($tag_string, $tag) {
-        return $tag_string . PHP_EOL . $tag;
-      }, ''), $layout);
-    }
-
-    return $layout;
-  }
-
-  protected function load($file)
-  {
-    foreach ($this->params as $key => $value) {
-      $$key = $value;
-    }
-    ob_start();
-    include_once($file);
-    return ob_get_clean();
-  }
-
-  protected function get_layout($layout)
-  {
-    return $this->load("{$this->views}/layouts/{$layout}.php");
-  }
-
-  protected function get_view($view)
-  {
-    return $this->load("{$this->views}/$view.php");
+    return $twig->render("$view.twig", $params);
   }
 }
